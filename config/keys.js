d9ed0c4b0e0434f907ffc6275dae50eafbaabd27
Brittney Fortier <brittfort34@gmail.com>
2018-10-31 03:03:12 -0400

    Created a User model and the ability to register a user

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://britt:asdf1234@ds147073.mlab.com:47073/devconnect"
+};
---
+++ server.js
@@ -0,0 +1,39 @@
+// Bring in express, body-parser, and mongoose
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+// Bring in routes
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
+// Create an application that will use express
+const app = express();
+
+// Body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+// Make sure the server is working
+app.get("/", (req, res) => res.send("Hello World"));
+
+// Use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
+// Assign the port to a variable, port
+const port = process.env.PORT || 5000;
+
+// Connect to the server
+app.listen(port, () => console.log(`Server running on port ${port}`));
