c284d3923c6a32b3a62f02536b0628feb698ae63
Brittney Fortier <brittfort34@gmail.com>
2018-11-11 23:49:41 -0500

    Instal Redux and create authentication/errors reducers and authentication actions

---
+++ client/src/validation/isEmpty.js
@@ -0,0 +1,9 @@
+// A global function that checks for errors
+
+const isEmpty = value =>
+  value === undefined ||
+  value === null ||
+  (typeof value === "object" && Object.keys(value).length === 0) ||
+  (typeof value === "string" && value.trim().length === 0);
+
+export default isEmpty;
