c96b12ea6813833c65ac0f92af438a0b76680b6c
Brittney Fortier <brittfort34@gmail.com>
2018-12-29 13:31:59 -0500

    Create a component that allowed the user to submit a post

---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,25 @@
+// Chrome or parent wrapper -- in return
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-router';
+import PostForm from './PostForm';
+import Spinner from '../common/Spinner';
+
+class Posts extends Component {
+  render() {
+    return (
+      <div className="feed">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <PostForm />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Posts;
